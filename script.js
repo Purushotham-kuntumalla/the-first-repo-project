c3d3b8ea59697cf0b52731cb3ef269f290b1077f
Purushotham-kuntumalla <kuntumallapurushotham2002@gmail.com>
2023-08-11 18:03:53 +0530

    the updated js file for the practice questions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Given an array of strings, use the filter() method to create a new array containing only strings with a length greater than 5 characters.
 
 let body = document.querySelector('body');
@@ -69,8 +70,77 @@ console.log(concatMethodOutPut)
 /*Question: Given an array of numbers, use the splice() method to remove all negative numbers from the array.
 Question: Given an array of strings, use the splice() method to replace the second and third strings with a new string. */
 
-let arrayNumbers = [1, 2, 3, 4, 5,-1,-2,-3]
-function negative(arrayNumbers) {
-    arrayNumbers
-}
-console.log(result)
+
+
+
+//Given an array of numbers, use the map() method to transform each number into its square.
+
+let arrayOfNumber = [1,2,3,4,5,6,7,8,9];
+let i = 2
+let mapping = arrayOfNumber.map((item) =>{
+    return item * item
+})
+console.log(mapping)
+//output [1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+
+//Given an array of user objects with name and age properties, transform it using the map() method to create an array of strings in the format "Name (Age)".
+
+let arrayOfPersons = [
+    {name : 'denver', age: 40},
+    {name : 'santoor',age : 60},
+    {name : 'rexona', age: 30},
+    {name : 'cinthol',age : 30},
+    {name : 'no 1', age: 10},
+    {name : 'shampoo',age : 15},
+    {name : 'pearce', age : 25},
+    {name : 'ice-cream',age : 35},
+    {name : 'bar', age : 45},
+    {name : 'blender sprite', age : 55},
+    {name : 'magic movements', age : 65},
+    {name : '100pipes', age : 75},
+    {name : 'odka', age : 15},
+    {name : 'mango juice', age : 25}
+]
+
+let transform = arrayOfPersons.map((name) => `${name.name} (${name.age})`);
+    console.log(transform)
+    
+/*output ['denver (40)', 'santoor (60)', 'rexona (30)', 'cinthol (30)', 'no 1 (10)', 'shampoo (15)', 'pearce (25)', 'ice-cream (35)', 'bar (45)', 'blender sprite (55)', 'magic movements (65)', '100pipes (75)', 'odka (15)', 'mango juice (25)']
+
+
+
+//Given an array of objects representing books with properties title, author, and genre, use the filter() method to create an array of books by a specific author
+
+let arrayOfBooks = [
+    {title : 'Ramayana', author : 'Valmiki', genre : 'Itihasa narratives of past events'},
+    {title : 'Mahabharata', author : 'Vyasa', genre : 'epic poem'},
+    {title : 'Bhagavad Gita', author : 'Vyasa', genre : 'epic poem'},
+    {title : 'Brahmasutras', author : 'Vyasa', genre : 'epic poem'},
+    {title : 'Nasavu nenu sastha neek endhuku', author : 'Nene', genre : 'about life'}
+]
+
+let filterBooks = arrayOfBooks.filter(author => { 
+    return author.author === 'Vyasa'
+})
+console.log(filterBooks)
+*/
+
+//Given an array of strings, use the map() method to convert each string to uppercase.
+
+let arrayOfConvert = ['pavan','nithish','naveen'];
+let conversion = arrayOfConvert.map((string) => {
+    return string.toUpperCase()
+})
+console.log(conversion)
+
+//output  (3) ['PAVAN', 'NITHISH', 'NAVEEN']
+
+//Given an array of strings, sort them based on their length using the sort() method.
+
+const strings = ['pavan','nithish','naveen','purushotham'];
+
+strings.sort((a, b) => a.length - b.length);
+
+console.log(strings);
+// Output:  ['pavan', 'naveen', 'nithish', 'purushotham']
